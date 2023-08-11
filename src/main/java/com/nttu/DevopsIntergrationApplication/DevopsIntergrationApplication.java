b021fcc7b01196c6bc43dfd2ebac0512d4667ca1
nttu <nttugpt@gmail.com>
2023-08-11 12:07:48 +0700

    First commit

---
+++ src/main/java/com/nttu/DevopsIntergrationApplication/DevopsIntergrationApplication.java
@@ -0,0 +1,13 @@
+package com.nttu.DevopsIntergrationApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsIntergrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsIntergrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttu/DevopsIntergrationApplication/DevopsIntergrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttu.DevopsIntergrationApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsIntergrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
